2780df088af4f9d1d9ba44dce9ff055a63134435
ahmad-codes-ai <ahmad.codes.x@gmail.com>
2026-07-10 23:39:24 +0500

    Added view,search and two helper functions update function still left. Helper functions testing needed

--- main.py
+++ main.py
@@ -59,6 +59,72 @@ def add():
          'stock':stock, 'sold':sold}
     inventory.append(d)
 
+def view():
+    for i in inventory:
+        print(i)
+
+def search():
+    print('1: Search with id: ')
+    print('2: Search with name: ')
+    s = int(input("Enter your choice: "))
+    found = False
+    if s == 1:
+        id = int(input("Enter id: "))
+        for i in inventory:
+            if i['id'] == id:
+                found = True
+                print(i)
+    if s == 2:
+        name = input("Enter name of product: ")
+        for i in inventory:
+            if i['name'].lower().strip() == name.lower().strip():
+                found = True
+                print(i)
+    if not found:
+        print("No items with this data exist")
+
+def item_exist(id):
+    '''This function is a helper function that will not print something just will return
+    True is the product is found in the inventory only take id as input. It is created to 
+    not write seraching logic again and again in update/restock/sell functions'''
+
+    found = False
+    for i in inventory:
+        if i['id'] == id:
+            found = True
+    if found:
+        return True
+    else:
+        return False
+    
+def item_index(id):
+    '''This is a helper function that will return the index position of an item found
+    in the inventory'''
+    idx = 0
+    for i in inventory:
+        if i['id'] == id:
+           return idx
+           break
+        else:
+            return False
+
+def update():
+    id = input("Enter id: ")
+    exist = item_exist(id)
+    if exist:
+        index = item_index(id)
+        print("1: Name")
+        print("2: Categry")
+        print("3: Cost Price")
+        print("4: Selling Price")
+        up = int(input("Enter your choice: "))
+        
+        if up == 1:
+            n_name = input("Enter new name: ")
+            inventory[index]['name'] = n_name        
+
+
+    
 
 while True:
     print('------------ Inventory Sales Managment System ------------')
@@ -82,6 +148,8 @@ while True:
 
         if user_2 == 1:
             add()
+        elif user_2 == 2:
+            view()
+        elif user_2 == 3:
+            search()
 
-
-print(inventory)
